695e82f35646212c4173bcaeaa8a02e848ad1ada
0x1eef <0x1eef@hardenedbsd.org>
2026-05-22 00:36:31 -0300

    utils: add LLM::Utils.{name_of,object_id}

--- mrblib/mruby-llm/utils.rb
+++ mrblib/mruby-llm/utils.rb
@@ -145,4 +145,33 @@ module LLM::Utils
     end
     value
   end
+
+  ##
+  # Returns the Ruby module or class name for an object.
+  #
+  # This bypasses overridden `#name` implementations by binding
+  # {Module#name} directly.
+  #
+  # @param [Module] obj
+  # @return [String, nil]
+  def name_of(obj)
+    ::Module.instance_method(:name).bind(obj).call
+  end
+
+  ##
+  # Renders the class-and-object-id portion of an inspect string.
+  #
+  # This returns strings like `LLM::Tool:0x1234abcd`, which can be
+  # embedded into custom inspect output.
+  #
+  # @param [Object] obj
+  # @return [String]
+  def object_id(obj)
+    klass = if Class === obj
+      name_of(obj) || name_of(obj.superclass) || obj.class.name
+    else
+      obj.class.name || obj.class.to_s
+    end
+    "#{klass}:0x#{obj.object_id.to_s(16)}"
+  end
 end
